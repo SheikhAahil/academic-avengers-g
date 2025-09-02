a1198d6d363c2a533892063bfdad90e015db61f0
SheikhAahil <aahilakhtar001@gmail.com>
2025-09-02 15:56:33 +0530

    Update filefilter.js

--- Backend/middleware/filefilter.js
+++ Backend/middleware/filefilter.js
@@ -1,7 +1,8 @@
-const allowedTypes = ['pdf','jpeg','img','jpg','xls','mp3'];
+
+const allowedTypes = ['pdf','jpeg','png','jpg','xls','mp3'];
+
 module.exports = (req, file, cb) => {
   const ext = file.originalname.split('.').pop().toLowerCase();
-  if (!allowedTypes.includes(ext)) return cb(null, false);
-  if(file.size > 7 * 1024 * 1024) return cb(null, false);
+  if (!allowedTypes.includes(ext)) return cb(new Error('File type not allowed'), false);
   cb(null, true);
 };
